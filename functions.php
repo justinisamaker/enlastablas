f3531e42e8e92c3d4c2f9a2ef4b252adb352c1f9
Justin Smith <justin@isamaker.com>
2019-12-12 17:39:28 -0600

    enqueue open sans

--- functions.php
+++ functions.php
@@ -1,3 +1,7 @@
 <?php
+  function adobo_add_google_fonts(){
+    wp_enqueue_style('adobo-google-fonts', 'http://fonts.googleapis.com/css?family=Open+Sans:400,700', false);
+  }
 
+  add_action('wp_enqueue_scripts', 'adobo_add_google_fonts');
 ?>
